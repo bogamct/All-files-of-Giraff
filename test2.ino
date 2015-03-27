ca006543e359310cd83fc5b057651ae48ce80955
vishnu_kanth <vishnukanth.mct@gmail.com>
2015-03-28 00:45:02 +0530

    2nd test

---
+++ test2.ino
@@ -0,0 +1,23 @@
+void setup()
+{
+  
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+
+
+void loop()
+{
+ 
+  if(Serial.available()>0)
+  {
+    char letter=Serial.read();
+  
+  if(letter=='1')
+  digitalWrite(13,HIGH);
+  if(letter=='5')
+  digitalWrite(13,LOW);
+}
+}
+
